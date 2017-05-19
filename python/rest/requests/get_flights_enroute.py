967a8a0f1561235ad4ca2e0a3a8ac6631c62d0dd
Jonathan Cone <jonathan.cone@fa-jonathancone.gwp.corp.flightaware.com>
2017-05-19 12:55:34 -0500

    Add python REST example
    	FAPI-1070

---
+++ python/rest/requests/get_flights_enroute.py
@@ -0,0 +1,16 @@
+import requests
+
+username = "YOUR_USERNAME"
+apiKey = "YOUR_API_KEY"
+fxmlUrl = "https://flightxml.flightaware.com/json/FlightXML3/"
+
+payload = {'airport_code':'KSFO', 'type':'enroute', 'howMany':'10'}
+response = requests.get(fxmlUrl + "AirportBoards", params=payload, auth=(username, apiKey))
+
+if response.status_code == 200:
+	print "Aircraft enroute to KSFO:"
+	decodedResponse = response.json()
+	for flight in decodedResponse['AirportBoardsResult']['enroute']['flights']:
+		print "{} ({})\t{} ({})".format(flight['ident'], flight['aircrafttype'], flight['origin']['airport_name'], flight['origin']['code'])
+else:
+	print "There was an error retrieving the data from the server."
