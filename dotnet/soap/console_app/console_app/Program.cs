fa62bb31fde1e1a709b8f64305040be03ef0bfff
Jonathan Cone <jonathan.cone@fa-jonathancone.gwp.corp.flightaware.com>
2017-05-24 12:40:42 -0500

    Chnage project name for dotnet soap example
    	FAPI-1070

---
+++ dotnet/soap/console_app/console_app/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using console_app.FlightXML3;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FlightXML3SoapClient client = new FlightXML3SoapClient();
+            client.ClientCredentials.UserName.UserName = "sampleUser";
+            client.ClientCredentials.UserName.Password = "abcdefghijklmnopqrstuvwxyz";
+
+            AirportBoardsRequest req = new AirportBoardsRequest();
+            req.airport_code = "KIAH";
+
+            AirportBoardsStruct res = client.AirportBoards("KIAH", false, "", "enroute", 10, 0);
+            Console.WriteLine("Flights enroute to KIAH");
+
+            foreach (var flight in res.enroute.flights)
+            {
+                Console.WriteLine(String.Format("{0} ({1})\t{2} ({3})", flight.ident, flight.aircrafttype,
+                    flight.origin.airport_name, flight.origin.code));
+            }
+        }
+    }
+}
